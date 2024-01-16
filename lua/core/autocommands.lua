01b5ce5ff24beb1909872d560f524fd7ceb4cec7
qfjz <red@qfj.pl>
2024-01-16 23:04:24 +0100

    2024-01-16-23:04:22

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -3,7 +3,7 @@ local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
 -- wyróżnienie kopiowanego tekstu
-autocmd('TextYankPost', {
+autocmd("TextYankPost", {
     group = augroup('HighlightYank', {}),
     pattern = '*',
     callback = function()
@@ -15,7 +15,7 @@ autocmd('TextYankPost', {
 })
 
  -- wyłącza parametry `cro` czyli nie wstawia automatycznie komentarza w kolejnej linii
-autocmd({ 'FileType', 'BufEnter' }, {
+autocmd({ "FileType", "BufEnter" }, {
     pattern = '*',
     command = [[setlocal formatoptions-=cro]]
 })
@@ -41,7 +41,22 @@ autocmd("FileType", {
     end,
 })
 
-autocmd({ "BufNewFile" }, {
+autocmd("BufEnter", {
+    group = augroup("iblenable", {}),
+    pattern = "*",
+    command = [[IBLEnable]]
+})
+
+autocmd("BufEnter", {
+    group = augroup("ibldisable", {}),
+    pattern = {
+        "*.norg",
+        "*.md",
+    },
+    command = [[IBLDisable]]
+})
+
+autocmd("BufNewFile", {
     pattern = { "*.sh" },
     command = [[0r ~/.config/nvim/templates/bash.sh]]
 })
