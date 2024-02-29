b2de1a31705c3bd9e698ee59fa4cd4cdeb56b6db
qfjz <red@qfj.pl>
2024-02-29 21:16:26 +0100

    2024-02-29-21:16:24

--- lua/config/pomo-cnf.lua
+++ lua/config/pomo-cnf.lua
@@ -6,19 +6,18 @@ configs.setup({
     -- How often the notifiers are updated.
     update_interval = 1000,
 
-    -- nie pokazuje powiadomień, informacje są wyświetlane w dolnej belce lualine
-    notifiers = {},
+    -- nie pokazuje powiadomień, wyświetla informacje w dolnej belce lualine
+    -- notifiers = {}
 
-    -- powiadomienia w chmurce vim.notify
-    -- notifiers = {
-    --     {
-    --         name = "Default",
-    --         opts = {
-    --             sticky = true,
-    --             title_icon = "󱎫",
-    --             text_icon = "󰄉",
-    --         },
-    --     },
-    -- },
-
+    -- wyświetla standardowe powiadomienia w chmurce
+    notifiers = {
+        {
+            name = "Default",
+            opts = {
+                sticky = true,
+                title_icon = "󱎫",
+                text_icon = "󰄉",
+            },
+        },
+    },
 })
