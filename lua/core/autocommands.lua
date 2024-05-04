6f833d89dd211c050d17ef8af15bcc026e78eb87
qfjz <red@qfj.pl>
2024-05-04 18:11:32 +0200

    2024-05-04-18:11:30

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -21,8 +21,15 @@ autocmd({ "FileType", "BufEnter" }, {
 })
 
 -- Ustawia kursor tam gdzie go zostawiliśmy
+-- https://github.com/adibhanna/nvim/blob/main/lua/config/autocmds.lua#L28
 autocmd("BufReadPost", {
-    command = [[if line("'\"") > 0 && line("'\"") <= line("$") | exe "normal! g'\"" | endif]]
+    callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        local lcount = vim.api.nvim_buf_line_count(0)
+        if mark[1] > 0 and mark[1] <= lcount then
+            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        end
+    end,
 })
 
 -- zamyka pliki przy pomocy 'q'
