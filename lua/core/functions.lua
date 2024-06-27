fe05103806685479101ec702d9a77fe5b58b41db
qfjz <red@qfj.pl>
2024-06-27 09:25:49 +0200

    2024-06-27-09:25:47

--- lua/core/functions.lua
+++ lua/core/functions.lua
@@ -581,6 +581,8 @@ Komendy = function()
         "language en_US",
         "language pl_PL.UTF-8",
         "LastMsg",
+        "Lazy clean",
+        "Lazy install",
         "Lazy update",
         "Light",
         "List",
@@ -832,4 +834,18 @@ Write = function()
         vim.cmd("silent write")
         vim.notify("Utworzyłem" .. " " .. vim.fn.expand("%:p"))
     end
+end
+
+Options = function()
+    local all_options = vim.api.nvim_get_all_options_info()
+    local win_number = vim.api.nvim_get_current_win()
+    local v = vim.wo[win_number]
+    local all_options = vim.api.nvim_get_all_options_info()
+    local result = ""
+    for key, val in pairs(all_options) do
+        if val.global_local == false and val.scope == "win" then
+            result = result .. "\n" .. key .. "=" .. tostring(v[key] or "<not set>")
+        end
+    end
+    print(result)
 end
